956fef483a42e24672b17cff994449693d0e040b
たかはしだいき <k022g0073@g.ne.jp>
2024-02-19 15:33:25 +0900

    3-1

--- StageScene.cpp
+++ StageScene.cpp
@@ -24,25 +24,25 @@ void StageScene::Update()
 	enemy.x += moveE.x;
 
 	if (enemy.x <= 0) {
-		moveE.x += 3;
+		moveE.x += 5;
 	}
 
 	if (enemy.x >= 1280) {
-		moveE.x -= 3;
+		moveE.x -= 5;
 	}
 
 	if (keys_[DIK_A]) {
-		moveP.x = -3;
+		moveP.x = -5;
 	}
 	else if (keys_[DIK_D]) {
-		moveP.x = 3;
+		moveP.x = 5;
 	}
 
 	if (keys_[DIK_W]) {
-		moveP.y = -3;
+		moveP.y = -5;
 	}
 	else if (keys_[DIK_S]) {
-		moveP.y = 3;
+		moveP.y = 5;
 	}
 
 	if (keys_[DIK_SPACE] && preKeys_[DIK_SPACE] == 0) {
@@ -68,7 +68,7 @@ void StageScene::Update()
 
 void StageScene::Draw()
 {
-	Novice::DrawEllipse((int)enemy.x, (int)enemy.y, (int)radius, (int)radius, 0.0f, RED, kFillModeSolid);
+	Novice::DrawBox((int)enemy.x, (int)enemy.y, (int)radius, (int)radius, 0.0f, RED, kFillModeSolid);
 
 	Novice::DrawEllipse((int)bullet.x, (int)bullet.y, (int)radius, (int)radius, 0.0f, BLUE, kFillModeSolid);
 
